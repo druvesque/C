50ff604f02d665aaaed3ee6ca6c21ef488ccfca2
druvesque <dhruvsachdeva6480@gmail.com>
2025-12-28 14:16:16 +0530

    my

--- mylib/druvesque.h
+++ mylib/druvesque.h
@@ -1,6 +1,8 @@
 // FUNC: swap
 //
-//
+// This function swaps the values of the two variables. 
+// The reference of the two variables is provided as 
+// the input.
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -9,7 +11,9 @@ void swap(int *a, int *b) {
 
 // FUNC: display_array
 //
-//
+// This function prints an array.
+// Inputs: pointer to the first element of the array
+//         size of the array
 void print_array(int arr[], int size) {
     printf("Current Array:\t");
     for (int i = 0; i < size; i++)
