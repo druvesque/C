8471f246bc6f6a5a5ab540ca4d20b340dfd00807
druvesque <dhruvsachdeva6480@gmail.com>
2025-12-29 11:06:34 +0530

    practice

--- mylib/druvesque.h
+++ mylib/druvesque.h
@@ -3,6 +3,7 @@
 // This function swaps the values of the two variables. 
 // The reference of the two variables is provided as 
 // the input.
+
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -14,8 +15,31 @@ void swap(int *a, int *b) {
 // This function prints an array.
 // Inputs: pointer to the first element of the array
 //         size of the array
+
 void print_array(int arr[], int size) {
     printf("Current Array:\t");
     for (int i = 0; i < size; i++)
         printf("%d\t", arr[i]);
+}
+
+// FUNC: bubble_sort
+//
+// Function to sort an array using bubble sort algorithm
+// Inputs: pointer to the first element of the array
+//         size of the array
+
+void bubble_sort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++)
+        for (int j = 0; j < size - i - 1; j++)
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+}
+
+// FUNC: selection_sort
+//
+//
+//
+
+void selection_sort(int arr[], int size) {
+
 }
